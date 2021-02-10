6173d25a7249d605c09abc41a00c468d88ee771b
claudio Nhanga <claudionhangapc@gmail.com>
2021-02-09 23:14:28 -0300

    criado modal projeto adição rapida

--- users/assets/js/app.js
+++ users/assets/js/app.js
@@ -2,6 +2,18 @@ const app = new Vue({
   el:"#app",
   data:{
     activate:false,
+    activeTarefa:false,
+    activaTarefaRapida:false,
+    activaModalProjeto:false,
+    tasks:[
+      {id:1,titulo:'Trabalhando com java', projeto:'programação',data_execucao:'Domingo, Fevereiro 17',categoria:'Categoria'},
+      {id:2,titulo:'Trabalhando com java', projeto:'programação',data_execucao:'Domingo, Fevereiro 17',categoria:'Categoria'},
+      {id:3,titulo:'Trabalhando com java', projeto:'programação',data_execucao:'Domingo, Fevereiro 17',categoria:'Categoria'}
+    ]
+  },
+  methods:{
+    
   }
+  
 });
 
